ae6769f2e169349f2199b894bb403dfc8a94ac69
Lars Dekkers <125201942+larsdekkers@users.noreply.github.com>
2024-12-11 14:50:52 +0100

    change to save textbox content

--- PeterFile.py
+++ PeterFile.py
@@ -1,8 +1,8 @@
 import pygame #for handeling most inputs and the window
 import json # for storing data between uses
 import drawmap # module for plotting on the pycanvas
-import ImageConverter # module for 
-import Widgetmanager
+import ImageConverter # module for creating a list of colors from a png/jpg
+import Widgetmanager # module for eazy creation of widgets
 
 pygame.init()
 
@@ -14,6 +14,7 @@ image = "testwinkel.png"
 running = True
 selectedColor = 0
 roomSize = [0,0]
+datafile = {}
 
 def Dataread() :
     'get all data out of a jsonfile'
@@ -22,6 +23,7 @@ def Dataread() :
     global roomMap
     global roomSize
     global squaresize
+    global inputBoxes
     with open(jsonName) as fp: #open the jsonfile
         datafile = json.load(fp)
     colors = datafile['colors'] #get the colors
@@ -29,12 +31,25 @@ def Dataread() :
     squaresize = (canvasWidth - 50)//len(roomMap[0])
     roomSize[0] = len(roomMap)*squaresize
     roomSize[1] = len(roomMap[0])*squaresize
+    inputBoxes = datafile["InputBoxes"]
 
 def DataStore() :
     'put data into a json file'
-    datafile['timesrun'] = datafile['timesrun']+ 1
-    datafile['map'] = roomMap
+    datafile['timesrun'] = datafile['timesrun'] + 1
+
     datafile['colors'] = colors
+    
+    inputboxes = {}
+    itemnumber = 0
+    for inputBox in Widgetmanager.widgets :
+        if type(inputBox[1]).__name__ == "InputBox" :
+            inputboxes[str(itemnumber)] = inputBox[1].text
+            itemnumber += 1
+    
+    datafile["InputBoxes"] = inputboxes
+    datafile['map'] = roomMap
+    
+            
     with open(jsonName, "w") as outfile : # save the json with updated info
         json.dump(datafile, outfile)
 
@@ -44,11 +59,18 @@ def Initialisemap() :
     global roomSize
     global colors
     global squaresize
+    global inputBoxes
+    global datafile
     roomMap = ImageConverter.Load(image)
     colors = ImageConverter.Colors
     squaresize = (canvasWidth - 50)//len(roomMap[0])
     roomSize[0] = len(roomMap)*squaresize #getting the width of the map
-    roomSize[1] = len(roomMap[0])*squaresize # gettig the height of the map
+    roomSize[1] = len(roomMap[0])*squaresize # getting the height of the map
+    inputBoxes = {}
+    inputBoxes["0"] = "wall"
+    inputBoxes["1"] = "path"
+    textbox1.text = inputBoxes["0"]
+    textbox2.text = inputBoxes["1"]
 
 def PixelToSquare(x : int,y : int)  -> tuple:
     "converts pixelcoordinates to coordinates on the map"
@@ -79,7 +101,6 @@ def KeyDown(event) :
     elif letter == "-" :
         ChangeSelectedColor(selectedColor-1)
 
-
 def ResetMap() :
     "resets the map back to the original image"
     Initialisemap()
@@ -89,8 +110,8 @@ def ResetMap() :
 def ChangeSelectedColor(color) :
     "changes the selected color and displays it on the canvas"
     global selectedColor
-    color = max(color, 0)
-    color = min(color, len(colors)-1)
+    color = max(color, 0) # make sure it cant go below 0
+    color = min(color, len(colors)-1) # make sure it cant go above the amount of colors
     selectedColor = color
     Drawmap.DrawRect(colorRect, colors[f"{color}"])
 
@@ -101,11 +122,14 @@ Drawmap.DrawFullMap()
 resetButton = Widgetmanager.Button((canvasWidth-50,0), (50,50), (255,0,0), Drawmap, ResetMap, "reset")
 colorRect = pygame.Rect(canvasWidth-50, 50, 50, 50) # the rectangle that shows the selected color
 
-Drawmap.DrawRect(colorRect, colors[f"{selectedColor}"])
-Drawmap.DrawText("use + and - to change colors", (canvasWidth//2-150, roomSize[1]+25), (255,255,255))
+Drawmap.DrawRect(colorRect, colors[f"{selectedColor}"]) # drawing the color
+Drawmap.DrawText("use + and - to draw different colors", (canvasWidth//2-180, roomSize[1]+25), (255,255,255)) # drawing text with instructions
 
-textboxtest = Widgetmanager.InputBox((30, canvasHeight-100), (150,30), Drawmap, 13)
-textboxtest2 = Widgetmanager.InputBox((30, canvasHeight-60), (150,30), Drawmap, 13)
+textbox1 = Widgetmanager.InputBox((30, canvasHeight-100), (150,30), Drawmap, 13, inputBoxes["0"])
+textbox2 = Widgetmanager.InputBox((30, canvasHeight-60), (150,30), Drawmap, 13, inputBoxes["1"])
+
+textbox1Color = Drawmap.DrawRect(pygame.Rect(180,canvasHeight-100,30,30), colors["0"])
+textbox2Color = Drawmap.DrawRect(pygame.Rect(180,canvasHeight-60,30,30), colors["1"])
 
 while running :
     pygame.display.update()
