6eb141cc5f63a8bfab44dc02825f3111d3c6f33d
Lars Dekkers <125201942+larsdekkers@users.noreply.github.com>
2024-12-11 14:55:20 +0100

    clearer comments

--- LoadImage.py
+++ LoadImage.py
@@ -1,14 +1,14 @@
 from PIL import Image
 def Load(imageName : str) -> list:
     "returns a nested list with rbg values [y][x](r,g,b)"
-    image = Image.open(f'C:/Users/2007080801/OneDrive - Rodenborch College/Math/Peter/{imageName}')
+    image = Image.open(f'C:/Users/2007080801/OneDrive - Rodenborch College/Math/Peter/{imageName}') #change path for it to work for you
 
-    pixels = list(image.getdata()) #get data in a list
+    pixels = list(image.getdata()) #get color data in a flat list
     width, height = image.size
-    pixels = [pixels[i * width:(i + 1) * width] for i in range(height)] # convert to nested list
+    pixels = [pixels[i * width:(i + 1) * width] for i in range(height)] # convert to nested list to return
 
-   #remove the opacity
+   #remove the opacity because it is not needed
     for row in range(len(pixels)) :
         for item in range(len(pixels[row])) :
-            pixels[row][item] = pixels[row][item][:-1]
+            pixels[row][item] = pixels[row][item][:-1] #remove the last element of all items
     return pixels
